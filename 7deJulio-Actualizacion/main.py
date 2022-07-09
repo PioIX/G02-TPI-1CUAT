51684df2eb7910c48c24c9c6c0f93a9291637b93
MatiasOjea <100933827+MatiasOjea@users.noreply.github.com>
2022-07-09 05:01:02 -0300

    Add files via upload

--- 7deJulio-Actualizacion/main.py
+++ 7deJulio-Actualizacion/main.py
@@ -5,18 +5,30 @@ app = Flask(__name__)
 
 def añadirBaseDeDatos(nom, con):
   conn = sqlite3.connect('baseDeDatos.db')
-  q = f"""INSERT OR IGNORE INTO Personas (nombre, contraseña, puntaje) VALUES ('{nom}', '{con}', 0)"""
-  conn.execute(q)
-  conn.commit()
+  q = f"""SELECT id FROM Personas WHERE nombre = '{nom}' AND contraseña = '{con}'"""
+  resu = conn.execute(q)
+  if resu.fetchone():
+    pass
+  else:
+    q = f"""INSERT INTO Personas (nombre, contraseña, puntaje) VALUES ('{nom}', '{con}', 0)"""
+    conn.execute(q)
+    conn.commit()
   conn.close()
+
+#q=f"""INSERT INTO Preguntas (pregunta, nOds) VALUES ('En el mundo el __________ % de la población vive en la pobreza, las cuales con menos de __________ dólares al día.', 1) """
+#q=f"""INSERT INTO Preguntas (pregunta, nOds) VALUES ('¿A qué hacen referencia las imagenes?', 1)"""
+#q=f"""INSERT INTO Preguntas (pregunta, nOds) VALUES ('¿Cual es el objetivo principal del ODS3?', 3)"""
+#q=f"""INSERT INTO Preguntas (pregunta, nOds) VALUES ('Para lograr este objetivo es necesario que los __________ esenciales sean asequibles.', 3)"""
+#q=f"""INSERT INTO Preguntas (pregunta, nOds) VALUES ('¿A qué hace referencia las imagenes?', 3)"""
+#q=f"""INSERT INTO Preguntas (pregunta, nOds) VALUES ('La pandemia del COVID-19 ha puesto de manifiesto la importancia de:', 6)"""
 
 def devolverUsuarios():
   conn = sqlite3.connect('baseDeDatos.db')
   q = f"""SELECT nombre, contraseña FROM Personas"""
   resu = conn.execute(q)
-  listaUsuarios = []
+  listaUsuarios = {}
   for linea in resu:
-    listaUsuarios.append(linea)
+    listaUsuarios[linea[0]] = linea[1]
   conn.close()
   return listaUsuarios
 
@@ -38,10 +50,10 @@ def irAPuntajes():
 
 @app.route('/registro')
 def irARegistro():
-  return render_template("registro.html")
-
+  return render_template("registro.html", listaUsuarios = devolverUsuarios())
+                                                                             
 @app.route('/juego', methods=['POST'])
-def irAJuego():
+def irAJuego():  
   nombreUsuario = request.form['nombre']
   contraseñaUsuario = request.form['contraseña']
   añadirBaseDeDatos(nombreUsuario, contraseñaUsuario)
